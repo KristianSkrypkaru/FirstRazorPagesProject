40c74c1c1a403ccc73ace068210b85ce5c70e5ab
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-02-22 22:27:15 +0200

    Add project files.

---
+++ FirstRazorPagesProject/Pages/Employees/Employees.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstRazorPagesProject.Pages.Employees
+{
+    public class EmployeesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FirstRazorPagesProject/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstRazorPagesProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string ?Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = $"Welcome to my house\n The time now {DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
