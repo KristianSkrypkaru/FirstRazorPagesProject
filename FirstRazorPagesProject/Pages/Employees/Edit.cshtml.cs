7d5f507087505cc4f286a8a365a5c8739c782a6e
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-03-29 23:03:54 +0300

    added implementation of the Edit method

--- FirstRazorPagesProject/Pages/Employees/Edit.cshtml.cs
+++ FirstRazorPagesProject/Pages/Employees/Edit.cshtml.cs
@@ -8,11 +8,12 @@ namespace FirstRazorPagesProject.Pages.Employees
     public class EditModel : PageModel
     {
         private readonly IEmloyeeRepository _emloyeeRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public EditModel(IEmloyeeRepository emloyeeRepository)
+        public EditModel(IEmloyeeRepository emloyeeRepository, IWebHostEnvironment webHostEnvironment)
         {
             _emloyeeRepository = emloyeeRepository;
-
+            _webHostEnvironment = webHostEnvironment;
         }
         public Employee Employee { get; set; }
         [BindProperty]
@@ -28,8 +29,35 @@ namespace FirstRazorPagesProject.Pages.Employees
         }
         public IActionResult OnPost(Employee employee) 
         {
+            if (Photo != null)
+            {
+                if (employee.PhotoPath != null)
+                {
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", employee.PhotoPath);
+                    System.IO.File.Delete(filePath);
+                }
+
+                employee.PhotoPath = ProcessUploadedFiel();
+            }
             Employee = _emloyeeRepository.Udate(employee);
             return RedirectToPage("Employees");
+        }
+
+        private string ProcessUploadedFiel()
+        {
+            string uniqueFileName = null;
+            if (Photo != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Photo.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
         }
     }
 }
